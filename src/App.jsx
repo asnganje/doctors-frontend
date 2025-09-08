8316ef74cfd3e66be92ea32ce4237c459d363afb
asnganje <abd.nganje@gmail.com>
2025-09-08 23:32:03 +0300

    setup basic vite up with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <section className="max-w-6xl mx-auto bg-gray-200 min-h-screen p-5">Welcome to the Doctors plaza!</section>
+    </>
+  )
+}
+
+export default App
