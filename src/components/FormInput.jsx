106992ffb396b79bd4a1c1f8ee765cde119a673a
asnganje <abd.nganje@gmail.com>
2025-09-10 14:22:43 +0300

    add form input component content

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,17 @@
-const FormInput = () => {
+const FormInput = ({label, onChange, placeholder, itemName, value, autoComplete, type="text"}) => {
   return(
-    <div className="">FormInput</div>
+    <div className="mb-4">
+      <label className="block text-left text-gray-700 font-medium mb-2">{label}</label>
+      <input
+      type= {type}
+      name={itemName}
+      autoComplete={autoComplete}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className="w-full px-4 py-2 border rounded-xl focus:ring-2 focus:ring-blue-500 outline-none"
+      />
+    </div>
   )
 }
 
