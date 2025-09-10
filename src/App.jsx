92cf00d099ab8a908fbefbdac6ba18f8b44779f3
asnganje <abd.nganje@gmail.com>
2025-09-10 07:58:11 +0300

    add normal routes in react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
 
@@ -7,6 +11,11 @@ function App() {
     <>
       <section className="max-w-6xl mx-auto bg-gray-100 shadow min-h-screen p-5 font-calligraphy">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/Signup" element={<Signup />}/>
+        </Routes>
       </section>
     </>
   )
