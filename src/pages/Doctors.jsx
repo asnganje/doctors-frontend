f160a5b2812350d3a6127ddc33bbd192fe605ed9
asnganje <abd.nganje@gmail.com>
2025-09-13 10:00:38 +0300

    show basic doctors list

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 const Doctors = () => {
+  const {user} = useSelector((state)=>state.auth)
+  const [successInfo, setSuccessInfo] = useState("")
+
+  useEffect(()=> {
+    if (user) {
+      setSuccessInfo("Logged in...")
+      const timer = setTimeout(()=> {
+      setSuccessInfo("")
+      }, 4000)
+      return () => clearTimeout(timer)
+    }
+  }, [user])
+  
   return(
-    <div className="">Doctors</div>
+    <div className="relative">
+      {successInfo && <div
+        className="absolute bottom-[-10] right-5 mb-4 italic px-2 rounded-lg text-center py-2 bg-green-100 text-green-700 text-sm font-medium shadow-sm animate-pulse"
+      >{successInfo}</div>}
+      Doctors
+    </div>
   )
 }
 
