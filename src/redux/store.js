cfc06366eecd22f24fb9eda4d52969c56a01bf0e
asnganje <abd.nganje@gmail.com>
2025-09-13 10:01:09 +0300

    modify store for data persistence

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit"
-import authReducer from "./slices/authSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage
+import authReducer from "./slices/authSlice";
 
-const store = configureStore(
-  {
-    reducer: {
-      auth: authReducer
-    }
-  }
-)
+const persistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["user", "token"],
+};
 
-export default store;
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/PAUSE",
+          "persist/FLUSH",
+          "persist/PURGE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
