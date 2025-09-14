844652c6e8ef827cc1350640d4785d746f42b525
asnganje <abd.nganje@gmail.com>
2025-09-14 16:54:26 +0300

    add component for doctor detail

--- src/pages/DoctorDetail.jsx
+++ src/pages/DoctorDetail.jsx
@@ -1,6 +1,21 @@
-const DoctorDetail = () => {
+const DoctorDetail = ({doctor}) => {
+  
   return(
-    <div className="">DoctorDetail</div>
+    <div
+      key={doctor.id}
+      className="bg-white p-6 rounded-2xl shadow-md hover:shadow-lg transition mb-6"
+    >
+      {doctor.picture_url && <img 
+        src={`http://localhost:3000${doctor.picture_url}`}
+        alt={doctor.name}
+        className="w-24 h-24 mx-auto rounded-full object-cover mb-4 border-2 border-amber-400"
+      />
+      }
+      <h3 className="text-xl font-semibold text-gray-700 text-center">{doctor.name}</h3>
+      <p className="text-gray-500 text-center">{doctor.specialization}</p>
+      <p className="text-gray-700 mt-2">{doctor.biography}</p>
+
+    </div>
   )
 }
 
