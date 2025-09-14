2e3950b5c81b50d057b90220aa49babe79c0d797
asnganje <abd.nganje@gmail.com>
2025-09-14 16:56:00 +0300

    add slice for doctors

---
+++ src/redux/slices/doctorsSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchDoctors } from "../thunks/doctorsThunk";
+import { addDoctor } from "../thunks/addDoctorThunk";
+
+const doctorsSlice = createSlice({
+  name: "doctors",
+  initialState: {
+    doctors: [],
+    loading: null,
+    error: null
+  },
+  reducers:{},
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchDoctors.pending, (state)=> {
+      state.loading = true;
+      state.error = null;
+    })
+    .addCase(fetchDoctors.fulfilled, (state, action)=> {
+      state.loading = false;
+      state.doctors = action.payload;
+      state.error = null;
+    })
+    .addCase(fetchDoctors.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload; 
+    })
+    .addCase(addDoctor.pending, (state) => {
+      state.loading = true;
+      state.error = null; 
+    })
+    .addCase(addDoctor.fulfilled, (state, action) => {
+      state.loading = false;
+      state.doctors.push(action.payload); 
+      state.error = null
+    })
+    .addCase(addDoctor.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    })
+  }
+})
+
+export default doctorsSlice.reducer 
