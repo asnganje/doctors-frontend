89f41be4662cc2ca90d8d1fe66fdc89c5f05b425
asnganje <abd.nganje@gmail.com>
2025-09-14 16:55:14 +0300

    pass doctors reducer to doctor slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage
 import authReducer from "./slices/authSlice";
+import doctorsReducer from "./slices/doctorsSlice";
 
 const persistConfig = {
   key: "auth",
@@ -9,11 +10,18 @@ const persistConfig = {
   whitelist: ["user", "token"],
 };
 
+const doctorsPersistConfig = {
+  key:"doctors",
+  storage,
+  whitelist: ["list"]
+}
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+const persistedDoctorsReducer = persistReducer(doctorsPersistConfig, doctorsReducer)
 
 export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
+    doctors: persistedDoctorsReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
