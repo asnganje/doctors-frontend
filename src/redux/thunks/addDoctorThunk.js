0f2f3a5395d4fa2ebca3b4a7605bb660e4cd2e19
asnganje <abd.nganje@gmail.com>
2025-09-14 16:56:10 +0300

    add doctor thunk

---
+++ src/redux/thunks/addDoctorThunk.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/v1/doctors"
+
+const addDoctor = createAsyncThunk("doctors/addDoctor",
+  async(doctorData, {rejectWithValue}) => {
+    
+    try {
+      const formData = new FormData()
+      formData.append("doctor[name]", doctorData.name)
+      formData.append("doctor[specialization]", doctorData.specialization)
+      formData.append("doctor[biography]", doctorData.biography)
+
+      if (doctorData.image) {
+        formData.append("doctor[image]", doctorData.image)        
+      }
+      
+      const response = await axios.post(API_URL, formData, {
+        headers:{
+          "Content-Type" : "multipart/form-data"
+        }
+      })
+
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Failed to add Doctor")
+    }
+  }
+)
+
+export {addDoctor}
