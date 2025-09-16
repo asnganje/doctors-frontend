302b6e8af0285257b84499dad36947aaa73abddf
asnganje <abd.nganje@gmail.com>
2025-09-16 06:56:44 +0300

    modify front end view with a button for delete

--- src/pages/DoctorDetail.jsx
+++ src/pages/DoctorDetail.jsx
@@ -1,12 +1,21 @@
+import { Trash2 } from 'lucide-react'
+import {useDispatch} from "react-redux"
+import { removeDoctor } from '../redux/thunks/doctorsThunk'
+
 const DoctorDetail = ({doctor}) => {
+  const dispatch = useDispatch()
+  const destroyHandler = () => {
+    dispatch(removeDoctor(doctor.id))
+  }
   
   return(
     <div
       key={doctor.id}
-      className="bg-white p-6 rounded-2xl shadow-md hover:shadow-lg transition mb-6"
+      className="relative bg-white p-6 rounded-2xl shadow-md hover:shadow-lg transition mb-6"
     >
       {doctor.picture_url && <img 
-        src={`https://rails-doctors-api-service.onrender.com${doctor.picture_url}`}
+      src={`http://localhost:3000/${doctor.picture_url}`}
+        // src={`https://rails-doctors-api-service.onrender.com${doctor.picture_url}`}
         alt={doctor.name}
         className="w-24 h-24 mx-auto rounded-full object-cover mb-4 border-2 border-amber-400"
       />
@@ -14,7 +23,9 @@ const DoctorDetail = ({doctor}) => {
       <h3 className="text-xl font-semibold text-gray-700 text-center">{doctor.name}</h3>
       <p className="text-gray-500 text-center">{doctor.specialization}</p>
       <p className="text-gray-700 mt-2">{doctor.biography}</p>
-
+      <button onClick={destroyHandler} className="absolute bottom-3 right-2">
+        <Trash2 className="cursor-pointer text-red-400 hover:text-red-500"/>
+      </button>
     </div>
   )
 }
