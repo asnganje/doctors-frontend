dc6fd6d4fb545cfc36b007025f2cfed4b7569c08
asnganje <abd.nganje@gmail.com>
2025-09-19 10:34:53 +0300

    fix add doctor to handle supabase

--- src/redux/thunks/addDoctorThunk.js
+++ src/redux/thunks/addDoctorThunk.js
@@ -1,29 +1,24 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// const API_URL = "http://localhost:3000/api/v1/doctors"
-const API_URL = "https://rails-doctors-api-service.onrender.com/api/v1/doctors"
+const API_URL = "http://localhost:3000/api/v1/doctors"
+// const API_URL = "https://rails-doctors-api-service.onrender.com/api/v1/doctors"
 
 const addDoctor = createAsyncThunk("doctors/addDoctor",
   async(doctorData, {rejectWithValue}) => {
     
     try {
-      const formData = new FormData()
-      formData.append("doctor[name]", doctorData.name)
-      formData.append("doctor[specialization]", doctorData.specialization)
-      formData.append("doctor[biography]", doctorData.biography)
-
-      if (doctorData.image) {
-        formData.append("doctor[image]", doctorData.image)        
-      }
-      
-      const response = await axios.post(API_URL, formData, {
-        headers:{
-          "Content-Type" : "multipart/form-data"
+      const payload = {
+        doctor: {
+          name: doctorData.name,
+          specialization: doctorData.specialization,
+          biography: doctorData.biography,
+          image_url: doctorData.image_url || null
         }
-      })
+      }       
 
-      return response.data
+      const response = await axios.post(API_URL, payload)
+      return response.data;
     } catch (error) {
       return rejectWithValue(error.response?.data || "Failed to add Doctor")
     }
