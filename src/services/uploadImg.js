52d485c7a04717eb2790c8855906d9b6aa2380c9
asnganje <abd.nganje@gmail.com>
2025-09-19 09:06:29 +0300

    add function for image upload

---
+++ src/services/uploadImg.js
@@ -0,0 +1,15 @@
+import { supabase } from "../lib/supabaseClient";
+
+export const uploadDoctorImg = async (file) => {
+  const fileName = `doctors/${crypto.randomUUID()}-${file.name}`
+  const {error} = await supabase.storage()
+    .from("doctor-images")
+    .upload(fileName, file)
+
+  if(error) throw error
+
+  const {data: publicUrlData} = supabase.storage()
+    .from("doctor-images")
+    .getPublicUrl(fileName)
+  return publicUrlData.publicUrl
+}
